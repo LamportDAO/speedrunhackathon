c12515342f273abea9cc0afa601cc7dcc523d60f
Irfan Asif <irfan_2020bece060@nitsri.net>
2023-01-10 14:21:59 +0530

    fix button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,7 +98,7 @@ export default function Home() {
             </Button>
           </HStack>
         </VStack>
-        <Center id='track'>
+        <Center>
           <Locations />
         </Center>
         <Center id='track'>
