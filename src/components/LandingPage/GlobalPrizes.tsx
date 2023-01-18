120fcc0fef9c42c184a3b370b122fbf105338cab
CMEONE <contact@kabirwearsato.ga>
2023-01-18 10:14:16 -0800

    Update tracks

--- src/components/LandingPage/GlobalPrizes.tsx
+++ src/components/LandingPage/GlobalPrizes.tsx
@@ -130,47 +130,34 @@ const GlobalPrizes = ({
         {isMobile ? (
           <CardMobile
             track={{
-              Name: 'Student Prize',
-              Sponsor: ['Solana University'],
-              PrizeWorth: 5000,
+              Name: 'LamportDAO Global Prize',
+              Sponsor: ['Eclipse'],
+              PrizeWorth: 2000,
               PrizeUnit: 'USDC',
-              PrizeDetails: ['2023 Breakpoint Tickets to the top student team'],
+              PrizeDetails: [
+                'Judges Favorite: $1,500',
+                'Most Popular: $500'
+              ],
               AboutUs:
-                'Solana U is a global community of curious coders and creatives that are dedicated to providing accessible blockchain education to all. As part of our mission, we strive to foster an exciting and interactive learning environment while providing resources and opportunities for students and educators to succeed in Web3. We welcome all individuals to join our diverse network and build with us on Solana.',
+                'Eclipse offers customizable rollups-as-a-service using the Solana virtual machine. This means you can spin up your own Cosmos chain and pick (1) the bytecode (SVM, EVM, soon Move), (2) the base layer (Celestia, Solana, Polygon, EigenLayer, NEAR), and (3) additional customizations for your execution layer. With your own Eclipse rollup, you can increase value capture for your application by adding MEV auctions, charging gas in your native token, subsidizing governance participation and more.',
               Description:
-                'Solana U is a global community of curious coders and creatives that are dedicated to providing accessible blockchain education to all. As part of our mission, we strive to foster an exciting and interactive learning environment while providing resources and opportunities for students and educators to succeed in Web3. We welcome all individuals to join our diverse network and build with us on Solana.',
+                'Sponsored by Eclipse, LamportDAO is awarding a Judge\'s Favorite and Most Popular prize to hackathon participants. These prizes are global and can be won alongside a specific track. Judges’ Favorites will be determined by voting among all the Judges. Most Popular will be determined by Reacts/Votes left on your Discord submission. May the best teams win!',
               Ideas: [''],
               Requirements:
-                "All members must be enrolled in University or Academic Institution. The project must be original and created within the hackathon time standards.\nWe will analyze all the student's projects individually using the following criteria:\n1. Originality: How original is the hack's concept?\n2.  Technology difficulty: How technically challenging was it to implement the hack?\n3.  Utility: How useful is the hack in everyday life? What kind of impact could it have and what additional value could it bring to the industry?\n4.  User Experience: Is the hack easy to use? Is the interface well-designed?\n5.  Market Fit:  Does the hack address a need or problem in the market? Is it likely to be successful in terms of adoption and usage?\n6.  Completion: Is the hack fully functional and complete? Did the team accomplish all of its goals?",
+                "Only open-sourced Github repos will be considered for Judges’ Favorite! Make sure you OS!",
               Links: [
                 {
-                  Title: 'Discord',
-                  Link: 'https://discord.com/invite/solanau',
-                },
-                {
-                  Title: 'Website',
-                  Link: 'https://www.solanau.org/',
-                },
-                {
                   Title: 'Twitter',
-                  Link: 'https://twitter.com/SolanaUni',
+                  Link: 'https://twitter.com/LamportDAO',
+                },
+                {
+                  Title: 'Discord',
+                  Link: 'https://sandstormhackathon.com/discord',
                 },
               ],
               Judges: [
                 {
-                  Name: 'Cleon',
-                  Twitter: '',
-                },
-                {
-                  Name: 'Dana Degenius',
-                  Twitter: '',
-                },
-                {
-                  Name: 'DonnySolana',
-                  Twitter: '',
-                },
-                {
-                  Name: 'Joe',
+                  Name: 'All Judges will be voting',
                   Twitter: '',
                 },
               ],
@@ -189,53 +176,38 @@ const GlobalPrizes = ({
             <CardDesktop
               activeTrackHandler={activeTrackHandler}
               track={{
-                Name: 'Student Prize',
-                Sponsor: ['Solana University'],
-                PrizeWorth: 5000,
-                PrizeUnit: 'USDC*',
-                PrizeDetails: [
-                  '2023 Breakpoint Tickets to the top student team',
-                ],
-                AboutUs:
-                  'Solana U is a global community of curious coders and creatives that are dedicated to providing accessible blockchain education to all. As part of our mission, we strive to foster an exciting and interactive learning environment while providing resources and opportunities for students and educators to succeed in Web3. We welcome all individuals to join our diverse network and build with us on Solana.',
-                Description:
-                  'Solana U is a global community of curious coders and creatives that are dedicated to providing accessible blockchain education to all. As part of our mission, we strive to foster an exciting and interactive learning environment while providing resources and opportunities for students and educators to succeed in Web3. We welcome all individuals to join our diverse network and build with us on Solana.',
-                Ideas: [''],
-                Requirements:
-                  "All members must be enrolled in University or Academic Institution. The project must be original and created within the hackathon time standards.\nWe will analyze all the student's projects individually using the following criteria:\n1. Originality: How original is the hack's concept?\n2.  Technology difficulty: How technically challenging was it to implement the hack?\n3.  Utility: How useful is the hack in everyday life? What kind of impact could it have and what additional value could it bring to the industry?\n4.  User Experience: Is the hack easy to use? Is the interface well-designed?\n5.  Market Fit:  Does the hack address a need or problem in the market? Is it likely to be successful in terms of adoption and usage?\n6.  Completion: Is the hack fully functional and complete? Did the team accomplish all of its goals?",
-                Links: [
-                  {
-                    Title: 'Discord',
-                    Link: 'https://discord.com/invite/solanau',
-                  },
-                  {
-                    Title: 'Website',
-                    Link: 'https://www.solanau.org/',
-                  },
-                  {
-                    Title: 'Twitter',
-                    Link: 'https://twitter.com/SolanaUni',
-                  },
-                ],
-                Judges: [
-                  {
-                    Name: 'Cleon',
-                    Twitter: '',
-                  },
-                  {
-                    Name: 'Dana Degenius',
-                    Twitter: '',
-                  },
-                  {
-                    Name: 'DonnySolana',
-                    Twitter: '',
-                  },
-                  {
-                    Name: 'Joe',
-                    Twitter: '',
-                  },
-                ],
-              }}
+              Name: 'LamportDAO Global Prize',
+              Sponsor: ['Eclipse'],
+              PrizeWorth: 2000,
+              PrizeUnit: 'USDC',
+              PrizeDetails: [
+                'Judges Favorite: $1,500',
+                'Most Popular: $500'
+              ],
+              AboutUs:
+                'Eclipse offers customizable rollups-as-a-service using the Solana virtual machine. This means you can spin up your own Cosmos chain and pick (1) the bytecode (SVM, EVM, soon Move), (2) the base layer (Celestia, Solana, Polygon, EigenLayer, NEAR), and (3) additional customizations for your execution layer. With your own Eclipse rollup, you can increase value capture for your application by adding MEV auctions, charging gas in your native token, subsidizing governance participation and more.',
+              Description:
+                'Sponsored by Eclipse, LamportDAO is awarding a Judge\'s Favorite and Most Popular prize to hackathon participants. These prizes are global and can be won alongside a specific track. Judges’ Favorites will be determined by voting among all the Judges. Most Popular will be determined by Reacts/Votes left on your Discord submission. May the best teams win!',
+              Ideas: [''],
+              Requirements:
+                "Only open-sourced Github repos will be considered for Judges’ Favorite! Make sure you OS!",
+              Links: [
+                {
+                  Title: 'Twitter',
+                  Link: 'https://twitter.com/LamportDAO',
+                },
+                {
+                  Title: 'Discord',
+                  Link: 'https://sandstormhackathon.com/discord',
+                },
+              ],
+              Judges: [
+                {
+                  Name: 'All Judges will be voting',
+                  Twitter: '',
+                },
+              ],
+            }}
             />
           </Center>
         )}
