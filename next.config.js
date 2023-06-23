9be85e0ca1eed7ba8ac94cfb6378b1c7ec7bcf5f
SolBlaze <contact@solblaze.org>
2023-06-23 00:16:39 -0700

    Add invite link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/Dpv5uGCT5E",
+        permanent: false
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
