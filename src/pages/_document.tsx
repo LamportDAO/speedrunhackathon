432e4ee51dc89a78b295bd205d02f6b78ded4d2b
Gabriele Picco <piccogabriele@gmail.com>
2023-06-26 10:44:45 +0200

    :sparkles: Optimize Images & Update Next/Font (#4)
    * :sparkles: Optimize Images & Update Next/Font
    
    * :coffin: Remove .idea folder
    
    * :memo: Update Readme
    
    * :ambulance: Add font pre-load

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link
+              rel="preload"
+              href="/fonts/joystix.woff2"
+              as="font"
+              crossOrigin=""
+              type="font/woff2"
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
